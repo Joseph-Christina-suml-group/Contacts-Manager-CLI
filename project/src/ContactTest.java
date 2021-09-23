5422e5a36c795d688cd9ffafc92c267242abe6a7
Joseph Congdon <jocongdon@yahoo.com>
2021-09-23 11:13:49 -0500

    beginning of the project

---
+++ project/src/ContactTest.java
@@ -0,0 +1,9 @@
+public class ContactTest {
+
+
+    public static void main(String[] args) {
+        Contact contact = new Contact("Norm Macdolald", "867-5309" );
+        System.out.println(contact.getName());
+        System.out.println(contact.getNumber());
+    }
+}
