146b2f863850fc874a120c113fcf512dd5287eb8
Joseph Congdon <jocongdon@yahoo.com>
2021-09-23 11:40:08 -0500

    first branch commit

--- project/src/ContactTest.java
+++ project/src/ContactTest.java
@@ -3,7 +3,7 @@ public class ContactTest {
 
     public static void main(String[] args) {
         Contact contact = new Contact("Norm Macdolald", "867-5309" );
-        Contact crewdog = new Contact("Crewdog", "588-2300");
+        Contact spade = new Contact("David Spade", "588-2300");
         System.out.println(contact.getName());
         System.out.println(contact.getNumber());
     }
